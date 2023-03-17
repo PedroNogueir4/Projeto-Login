af32e4f0099f1caccd80e922c3395e8206d11ccc
PedroNogueir4 <pedroffn209@gmail.com>
2023-03-17 17:41:37 -0300

    Simulação de banco de Dados realizada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import { React, useRef } from "react";
 
-import { Container, ContainerBottom, ContainerMain } from "./styles";
+import { Container, ContainerBottom, ContainerMain, Inputs } from "./styles";
 
 function App() {
 
-  const login = () => {
+  const inputEmail = useRef()
+  const inputPass = useRef()
 
+  const users =
+  {
+    name: 'Pedro',
+    id: 5,
+    email: 'simulaçãobancodedados@gmail.com',
+    senha: '123321kk'
+  }
+
+
+  const login = () => {
+    const user = {
+      email: inputEmail.current.value,
+      senha: inputPass.current.value
+    }
+    if (user.email && user.senha === users.email && users.senha) {
+
+      console.log('Login realizado com Sucesso')}
+      
+      
+      else {
+      console.log(" Email ou Senha são inválidos")
+    }
 
   }
 
@@ -16,10 +39,10 @@ function App() {
       <ContainerMain>
         <h3>Bem-vindo ao Login</h3>
         <p>Preencha os dados do usuário para acessar</p>
-        <input placeholder="Email"></input>
-        <input placeholder="Senha"></input>
+        <Inputs ref={inputEmail} placeholder="Email"></Inputs>
+        <Inputs ref={inputPass} placeholder="Senha" type="password"></Inputs>
         <a>Esqueceu a senha?</a>
-        <button type="button" onClick={login}>Entrar</button>
+        <button onClick={login} type="button">Entrar</button>
       </ContainerMain>
 
       <ContainerBottom><p>© 2023 Copyright</p></ContainerBottom>
--- src/styles.js
+++ src/styles.js
@@ -18,26 +18,18 @@ height: 70%;
 display: flex;
 flex-direction: column;
 align-items: center;
-gap: 29px;
 box-shadow: 0px 1px 20px rgba(0, 0, 0, 0.25);
 border-radius: 10px;
 
 p{
     font-size: 11px;
-}
-input{
-    height: 38px;
-    width: 73%;
-    outline: none;
-    border-radius: 14px;
-    padding-left: 6px;
-    border: 2px solid #cecece;
-    color: #8E8E8E;
+    margin: 5%;
 }
 
 button{
+    margin-top: 7%;
     width: 100%;
-    height: 60px;
+    height: 15%;
     background: #FF790E;
     border-radius: 5px;
     border: none;
@@ -48,6 +40,7 @@ button{
 a{
     cursor: pointer;
     font-size: 11.9px;
+    color: #696969;
 }
 a:hover {
     opacity: 0.5;
@@ -70,7 +63,16 @@ button:active {
 }
 
 `
-
+export const Inputs = styled.input`
+  height: 38px;
+    width: 89%;
+    outline: none;
+    border-radius: 14px;
+    padding-left: 6px;
+    border: 2px solid #cecece;
+    color: #8E8E8E;
+    margin:4%;
+`
 
 export const ContainerBottom = styled.div`
 background-color: #FF790E;
